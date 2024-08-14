8d134bcde1f7ba90c267547e853fb99ee8030d43
waseembaig <52992501+waseembaig@users.noreply.github.com>
2024-08-14 14:35:41 +0530

    update do.py

--- do.py
+++ do.py
@@ -125,7 +125,7 @@ def release():
     run(
         [
             py() + " -m pip install --upgrade twine",
-            "{} -m twine upload -u {} -p {} dist/*".format(
+            "{} -m twine upload -u {} -p {} dist/*.whl dist/*.tar.gz".format(
                 py(),
                 os.environ["PYPI_USERNAME"],
                 os.environ["PYPI_PASSWORD"],
