0c42cceab6d7dea5fa403d4e056a0727eef47a0f
waseembaig <52992501+waseembaig@users.noreply.github.com>
2024-08-28 16:12:01 +0530

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "snappi_ixload"
-version = "0.0.2"
+version = "0.0.3"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
